96df7fc8984202a5bde1d60c302555339f3c3948
root <manolkata4@abv.bg>
2023-04-27 17:27:27 +0300

    add missing object reference before method calls

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ int main()
 {
 	VoltageReader vr;
 
-	std::cout << getVoltage(0) << std::endl;
-	std::cout << getVoltage(1) << std::endl;
-	std::cout << getVoltage(2) << std::endl;
-	std::cout << getVoltage(3) << std::endl;
+	std::cout << vr.getVoltage(0) << std::endl;
+	std::cout << vr.getVoltage(1) << std::endl;
+	std::cout << vr.getVoltage(2) << std::endl;
+	std::cout << vr.getVoltage(3) << std::endl;
 	return 0;
 
 }
